43492e16c528a280d7fb2dc52eef1d27015211ce
damaris <mdamarisnduku@gmail.com>
2022-09-01 20:26:22 +0300

    commit tree e0adeaeb3c90cfc1ebf43d1bcf90aed755bfe7ee

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_array)# code an argument here
   # Your code here
+  dwarf_array.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls_array)# code an argument here
   # Your code here
+  calls_array.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_calls_array)# code an argument here
   # Your code here
+  long_calls_array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_exists)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
